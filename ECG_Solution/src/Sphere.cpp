6ad80bc59f33baee30d9eea0f57db468bbd6196f
Christopher Stanek <chris.stanek97@gmail.com>
2020-11-10 20:58:15 +0100

    Sphere: Indices generation done. Some of render, update, creation done.

--- ECG_Solution/src/Sphere.cpp
+++ ECG_Solution/src/Sphere.cpp
@@ -8,14 +8,119 @@ Sphere::Sphere(glm::vec3 center, float radius, glm::vec3 color, glm::vec3 scale,
 	m_radius(radius),
 	m_color(color),
 	m_sectorCount(sectorCount),
-	m_stackCount(stackCount)
+	m_stackCount(stackCount),
+	m_model_lq(0),
+	m_color_lq(4)
 {
 	createVertices();
 	createIndices();
+
+	
+	glGenBuffers(1, &_vboID); // vertex buffer
+	glBindBuffer(GL_ARRAY_BUFFER, _vboID);
+	glBufferData(GL_ARRAY_BUFFER,
+		m_vertices.size() * sizeof(float),
+		&m_vertices[0],
+		GL_STATIC_DRAW);
+
+	glGenBuffers(1, &_iboID); // index buffer
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _iboID);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+		m_indices.size() * sizeof(unsigned int),
+		&m_indices[0],
+		GL_STATIC_DRAW);
+}
+
+Sphere::~Sphere() {
+	// free VAO and VBO
+	glDeleteBuffers(1, &_vboID);
+	glDeleteBuffers(1, &_iboID);
+
+	glDeleteVertexArrays(1, &vao)
+}
+
+void Sphere::update()
+{
+	Actor::update();
+	glUniformMatrix4fv(m_model_lq, 1, GL_FALSE, glm::value_ptr(m_model));
+	glUniform3fv(m_color_lq, 1, glm::value_ptr(m_color));
+}
+
+void Sphere::render()
+{
+	// bind VBOs
+	glBindBuffer(GL_ARRAY_BUFFER, _vboID);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _iboID);
+
+	// activate attrib arrays
+	glEnableVertexAttribArray(attribVertex);
+	glEnableVertexAttribArray(attribNormal);
+	glEnableVertexAttribArray(attribTexCoord);
+
+	// set attrib arrays with stride and offset
+	int stride = sphere.getInterleavedStride();     // should be 32 bytes
+	glVertexAttribPointer(attribVertex, 3, GL_FLOAT, false, stride, (void*)0);
+	glVertexAttribPointer(attribNormal, 3, GL_FLOAT, false, stride, (void*)(sizeof(float) * 3));
+	glVertexAttribPointer(attribTexCoord, 2, GL_FLOAT, false, stride, (void*)(sizeof(float) * 6));
+
+	// draw a sphere with VBO
+	glDrawElements(GL_TRIANGLES,                    // primitive type
+		sphere.getIndexCount(),          // # of indices
+		GL_UNSIGNED_INT,                 // data type
+		(void*)0);                       // offset to indices
+
+// deactivate attrib arrays
+	glDisableVertexAttribArray(attribVertex);
+	glDisableVertexAttribArray(attribNormal);
+	glDisableVertexAttribArray(attribTexCoord);
+
+	// unbind VBOs
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	/*
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 3 * sizeof(float), &m_vertices[0]);
+
+	glDrawElements(GL_TRIANGLES, (unsigned int)m_indices.size(), GL_UNSIGNED_INT, m_indices.data());
+
+	glDisableClientState(GL_VERTEX_ARRAY);
+	*/
 }
 
 void Sphere::createIndices() {
+	unsigned int v1, v2;
+	for (int i = 0; i < m_stackCount; ++i) { // the first vertices are one stackAngle down. (so increase first)
+		v1 = i * m_sectorCount;
+		v2 = (i + 1) * m_sectorCount;
+		for (int j = 0; j < m_sectorCount; j++) {
 
+			// indices
+			//  v1--v1+1
+			//  | \  |
+			//  |  \ |
+			//  v2--v2+1
+			if (i != (m_stackCount - 1)) { // this is the corner to the left, we don't want this at the end
+				m_indices.push_back(v1 + j);
+				m_indices.push_back(v2 + j);
+				m_indices.push_back(v2 + j + 1);
+			}
+
+			if (i != 0) { // this is the corner to the right, we don't want this at the beginning
+				m_indices.push_back(v2 + j + 1);
+				m_indices.push_back(v1 + j + 1);
+				m_indices.push_back(v1 + j);
+			}
+		}
+	}
+}
+
+const unsigned int* Sphere::getIndices() {
+	return m_indices.data();
+}
+
+const unsigned int Sphere::getIndicesSize() {
+	return (unsigned int)m_indices.data() * sizeof(unsigned int);
 }
 
 void Sphere::createVertices() {
@@ -40,7 +145,14 @@ void Sphere::createVertices() {
 			m_vertices.push_back(x);
 			m_vertices.push_back(y);
 			m_vertices.push_back(z);
-
 		}
 	}
+}
+
+const float* Sphere::getVertices() {
+	return m_vertices.data();
+}
+
+const unsigned int Sphere::getVerticesSize() {
+	return (unsigned int)m_vertices.data() * sizeof(float);
 }
--- ECG_Solution/src/Sphere.h
+++ ECG_Solution/src/Sphere.h
@@ -1,5 +1,9 @@
 #pragma once
 #include "Actor.h"
+// Include GLEW. Always include it before gl.h and glfw3.h, since it's a bit magic.
+#include <GL/glew.h>
+// Include GLFW
+#include <GLFW/glfw3.h>
 #include <vector>
 class Sphere :
     public Actor
@@ -12,6 +16,14 @@ public:
     virtual void update() override;
     virtual void render() override;
 
+    const unsigned int* getIndices();
+    const unsigned int getIndicesSize();
+
+    const float* getVertices();
+    const unsigned int getVerticesSize();
+
+
+
 private:
     void createVertices();
     void createIndices();
@@ -19,7 +31,12 @@ private:
     float m_radius;
     glm::vec3 m_color;
     std::vector<float> m_vertices;
-    std::vector<float> m_indices;
+    std::vector<unsigned int> m_indices;
     int m_sectorCount;
     int m_stackCount;
+    const GLuint m_model_lq;
+    const GLuint m_color_lq;
+
+    GLuint _vboID;
+    GLuint _iboID;
 };
